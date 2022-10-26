d163d1bf4bd69b634fdc59da975098ee0f8bfd98
Tpierre31 <thierry.pierre@stonybrook.edu>
2022-10-26 00:55:18 -0400

    Commit

---
+++ sql_table_creation.py
@@ -0,0 +1,83 @@
+import dbm
+import pandas as pd 
+import sqlalchemy
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GCP_MYSQL_HOSTNAME = os.getenv("GCP_MYSQL_HOSTNAME")
+GCP_MYSQL_USER = os.getenv("GCP_MYSQL_USERNAME")
+GCP_MYSQL_PASSWORD = os.getenv("GCP_MYSQL_PASSWORD")
+GCP_MYSQL_DATABASE = os.getenv("GCP_MYSQL_DATABASE")
+
+connection_string_gcp = f'mysql+pymysql://{GCP_MYSQL_USER}:{GCP_MYSQL_PASSWORD}@{GCP_MYSQL_HOSTNAME}:3306/{GCP_MYSQL_DATABASE}'
+db_gcp = create_engine(connection_string_gcp)
+
+table_prod_patients = """
+create table if not exists production_patients (
+    id int auto_increment,
+    mrn varchar(255) default null unique,
+    first_name varchar(255) default null,
+    last_name varchar(255) default null,
+    zip_code varchar(255) default null,
+    dob varchar(255) default null,
+    gender varchar(255) default null,
+    contact_mobile varchar(255) default null,
+    contact_home varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+table_Medications = """
+create table if not exists production_patient_medications (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    med_ndc varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES production_patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (med_ndc) REFERENCES production_medications(med_ndc) ON DELETE CASCADE
+); 
+"""
+
+table_Treatments_Procedures = """
+create table if not exists production_patients (
+    id int auto_increment,
+    mrn varchar(255) default null unique,
+    first_name varchar(255) default null,
+    last_name varchar(255) default null,
+    zip_code varchar(255) default null,
+    dob varchar(255) default null,
+    gender varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+table_Condtions = """
+create table if not exists production_conditions (
+    id int auto_increment,
+    icd10_code varchar(255) default null unique,
+    icd10_description varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+table_SDoH = """
+create table if not exists production_patients (
+    id int auto_increment,
+    mrn varchar(255) default null unique,
+    first_name varchar(255) default null,
+    last_name varchar(255) default null,
+    zip_code varchar(255) default null,
+    dob varchar(255) default null,
+    gender varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+db_gcp.execute(table_prod_patients)
+db_gcp.execute(table_Medications)
+db_gcp.execute(table_Treatments_Procedures)
+db_gcp.execute(table_Condtions)
+db_gcp.execute(table_SDoH)
+
+gcp_tables = db_gcp.table_names()
